be555c4135348b8f46db1216e8be0a95e572c8a0
yigitkalan <sametykalan@gmail.com>
2023-07-19 14:27:43 +0300

    add the required horizantal movement code to original lean tocuh script as an addition

--- Assets/Plugins/CW/LeanTouch/Extras/Scripts/LeanDragTranslate.cs
+++ Assets/Plugins/CW/LeanTouch/Extras/Scripts/LeanDragTranslate.cs
@@ -3,183 +3,251 @@ using CW.Common;
 
 namespace Lean.Touch
 {
-	/// <summary>This component allows you to translate the current GameObject relative to the camera using the finger drag gesture.</summary>
-	[HelpURL(LeanTouch.HelpUrlPrefix + "LeanDragTranslate")]
-	[AddComponentMenu(LeanTouch.ComponentPathPrefix + "Drag Translate")]
-	public class LeanDragTranslate : MonoBehaviour
-	{
-		/// <summary>The method used to find fingers to use with this component. See LeanFingerFilter documentation for more information.</summary>
-		public LeanFingerFilter Use = new LeanFingerFilter(true);
+    /// <summary>This component allows you to translate the current GameObject relative to the camera using the finger drag gesture.</summary>
+    [HelpURL(LeanTouch.HelpUrlPrefix + "LeanDragTranslate")]
+    [AddComponentMenu(LeanTouch.ComponentPathPrefix + "Drag Translate")]
+    public class LeanDragTranslate : MonoBehaviour
+    {
+        [SerializeField]
+        private bool isOnlyX = false;
 
-		/// <summary>The camera the translation will be calculated using.
-		/// None/null = MainCamera.</summary>
-		public Camera Camera { set { _camera = value; } get { return _camera; } } [SerializeField] private Camera _camera;
+        /// <summary>The method used to find fingers to use with this component. See LeanFingerFilter documentation for more information.</summary>
+        public LeanFingerFilter Use = new LeanFingerFilter(true);
 
-		/// <summary>The movement speed will be multiplied by this.
-		/// -1 = Inverted Controls.</summary>
-		public float Sensitivity { set { sensitivity = value; } get { return sensitivity; } } [SerializeField] private float sensitivity = 1.0f;
+        /// <summary>The camera the translation will be calculated using.
+        /// None/null = MainCamera.</summary>
+        public Camera Camera
+        {
+            set { _camera = value; }
+            get { return _camera; }
+        }
 
-		/// <summary>If you want this component to change smoothly over time, then this allows you to control how quick the changes reach their target value.
-		/// -1 = Instantly change.
-		/// 1 = Slowly change.
-		/// 10 = Quickly change.</summary>
-		public float Damping { set { damping = value; } get { return damping; } } [SerializeField] protected float damping = -1.0f;
+        [SerializeField]
+        private Camera _camera;
 
-		/// <summary>This allows you to control how much momentum is retained when the dragging fingers are all released.
-		/// NOTE: This requires <b>Dampening</b> to be above 0.</summary>
-		public float Inertia { set { inertia = value; } get { return inertia; } } [SerializeField] [Range(0.0f, 1.0f)] private float inertia;
+        /// <summary>The movement speed will be multiplied by this.
+        /// -1 = Inverted Controls.</summary>
+        public float Sensitivity
+        {
+            set { sensitivity = value; }
+            get { return sensitivity; }
+        }
 
-		[SerializeField]
-		private Vector3 remainingTranslation;
+        [SerializeField]
+        private float sensitivity = 1.0f;
 
-		/// <summary>If you've set Use to ManuallyAddedFingers, then you can call this method to manually add a finger.</summary>
-		public void AddFinger(LeanFinger finger)
-		{
-			Use.AddFinger(finger);
-		}
+        /// <summary>If you want this component to change smoothly over time, then this allows you to control how quick the changes reach their target value.
+        /// -1 = Instantly change.
+        /// 1 = Slowly change.
+        /// 10 = Quickly change.</summary>
+        public float Damping
+        {
+            set { damping = value; }
+            get { return damping; }
+        }
 
-		/// <summary>If you've set Use to ManuallyAddedFingers, then you can call this method to manually remove a finger.</summary>
-		public void RemoveFinger(LeanFinger finger)
-		{
-			Use.RemoveFinger(finger);
-		}
+        [SerializeField]
+        protected float damping = -1.0f;
 
-		/// <summary>If you've set Use to ManuallyAddedFingers, then you can call this method to manually remove all fingers.</summary>
-		public void RemoveAllFingers()
-		{
-			Use.RemoveAllFingers();
-		}
+        /// <summary>This allows you to control how much momentum is retained when the dragging fingers are all released.
+        /// NOTE: This requires <b>Dampening</b> to be above 0.</summary>
+        public float Inertia
+        {
+            set { inertia = value; }
+            get { return inertia; }
+        }
+
+        [SerializeField]
+        [Range(0.0f, 1.0f)]
+        private float inertia;
+
+        [SerializeField]
+        private Vector3 remainingTranslation;
+
+        /// <summary>If you've set Use to ManuallyAddedFingers, then you can call this method to manually add a finger.</summary>
+        public void AddFinger(LeanFinger finger)
+        {
+            Use.AddFinger(finger);
+        }
+
+        /// <summary>If you've set Use to ManuallyAddedFingers, then you can call this method to manually remove a finger.</summary>
+        public void RemoveFinger(LeanFinger finger)
+        {
+            Use.RemoveFinger(finger);
+        }
+
+        /// <summary>If you've set Use to ManuallyAddedFingers, then you can call this method to manually remove all fingers.</summary>
+        public void RemoveAllFingers()
+        {
+            Use.RemoveAllFingers();
+        }
 
 #if UNITY_EDITOR
-		protected virtual void Reset()
-		{
-			Use.UpdateRequiredSelectable(gameObject);
-		}
+        protected virtual void Reset()
+        {
+            Use.UpdateRequiredSelectable(gameObject);
+        }
 #endif
 
-		protected virtual void Awake()
-		{
-			Use.UpdateRequiredSelectable(gameObject);
-		}
+        protected virtual void Awake()
+        {
+            Use.UpdateRequiredSelectable(gameObject);
+        }
 
-		protected virtual void Update()
-		{
-			// Store
-			var oldPosition = transform.localPosition;
+        protected virtual void Update()
+        {
+            // Store
+            var oldPosition = transform.localPosition;
 
-			// Get the fingers we want to use
-			var fingers = Use.UpdateAndGetFingers();
+            // Get the fingers we want to use
+            var fingers = Use.UpdateAndGetFingers();
 
-			// Calculate the screenDelta value based on these fingers
-			var screenDelta = LeanGesture.GetScreenDelta(fingers);
+            // Calculate the screenDelta value based on these fingers
+            var screenDelta = LeanGesture.GetScreenDelta(fingers);
 
-			if (screenDelta != Vector2.zero)
-			{
-				// Perform the translation
-				if (transform is RectTransform)
-				{
-					TranslateUI(screenDelta);
-				}
-				else
-				{
-					Translate(screenDelta);
-				}
-			}
+            if (isOnlyX)
+            {
+                screenDelta = new Vector2(screenDelta.x, 0);
+            }
 
-			// Increment
-			remainingTranslation += transform.localPosition - oldPosition;
+            if (screenDelta != Vector2.zero)
+            {
+                // Perform the translation
+                if (transform is RectTransform)
+                {
+                    TranslateUI(screenDelta);
+                }
+                else
+                {
+                    Translate(screenDelta);
+                }
+            }
 
-			// Get t value
-			var factor = CwHelper.DampenFactor(Damping, Time.deltaTime);
+            // Increment
+            remainingTranslation += transform.localPosition - oldPosition;
 
-			// Dampen remainingDelta
-			var newRemainingTranslation = Vector3.Lerp(remainingTranslation, Vector3.zero, factor);
+            // Get t value
+            var factor = CwHelper.DampenFactor(Damping, Time.deltaTime);
 
-			// Shift this transform by the change in delta
-			transform.localPosition = oldPosition + remainingTranslation - newRemainingTranslation;
+            // Dampen remainingDelta
+            var newRemainingTranslation = Vector3.Lerp(remainingTranslation, Vector3.zero, factor);
 
-			if (fingers.Count == 0 && inertia > 0.0f && Damping > 0.0f)
-			{
-				newRemainingTranslation = Vector3.Lerp(newRemainingTranslation, remainingTranslation, inertia);
-			}
+            // Shift this transform by the change in delta
+            transform.localPosition = oldPosition + remainingTranslation - newRemainingTranslation;
 
-			// Update remainingDelta with the dampened value
-			remainingTranslation = newRemainingTranslation;
-		}
+            if (fingers.Count == 0 && inertia > 0.0f && Damping > 0.0f)
+            {
+                newRemainingTranslation = Vector3.Lerp(
+                    newRemainingTranslation,
+                    remainingTranslation,
+                    inertia
+                );
+            }
 
-		private void TranslateUI(Vector2 screenDelta)
-		{
-			var finalCamera = _camera;
+            // Update remainingDelta with the dampened value
+            remainingTranslation = newRemainingTranslation;
+        }
 
-			if (finalCamera == null)
-			{
-				var canvas = transform.GetComponentInParent<Canvas>();
+        private void TranslateUI(Vector2 screenDelta)
+        {
+            var finalCamera = _camera;
 
-				if (canvas != null && canvas.renderMode != RenderMode.ScreenSpaceOverlay)
-				{
-					finalCamera = canvas.worldCamera;
-				}
-			}
+            if (finalCamera == null)
+            {
+                var canvas = transform.GetComponentInParent<Canvas>();
 
-			// Screen position of the transform
-			var screenPoint = RectTransformUtility.WorldToScreenPoint(finalCamera, transform.position);
+                if (canvas != null && canvas.renderMode != RenderMode.ScreenSpaceOverlay)
+                {
+                    finalCamera = canvas.worldCamera;
+                }
+            }
 
-			// Add the deltaPosition
-			screenPoint += screenDelta * Sensitivity;
+            // Screen position of the transform
+            var screenPoint = RectTransformUtility.WorldToScreenPoint(
+                finalCamera,
+                transform.position
+            );
 
-			// Convert back to world space
-			var worldPoint = default(Vector3);
+            // Add the deltaPosition
+            screenPoint += screenDelta * Sensitivity;
 
-			if (RectTransformUtility.ScreenPointToWorldPointInRectangle(transform.parent as RectTransform, screenPoint, finalCamera, out worldPoint) == true)
-			{
-				transform.position = worldPoint;
-			}
-		}
+            // Convert back to world space
+            var worldPoint = default(Vector3);
 
-		private void Translate(Vector2 screenDelta)
-		{
-			// Make sure the camera exists
-			var camera = CwHelper.GetCamera(this._camera, gameObject);
+            if (
+                RectTransformUtility.ScreenPointToWorldPointInRectangle(
+                    transform.parent as RectTransform,
+                    screenPoint,
+                    finalCamera,
+                    out worldPoint
+                ) == true
+            )
+            {
+                transform.position = worldPoint;
+            }
+        }
 
-			if (camera != null)
-			{
-				// Screen position of the transform
-				var screenPoint = camera.WorldToScreenPoint(transform.position);
+        private void Translate(Vector2 screenDelta)
+        {
+            // Make sure the camera exists
+            var camera = CwHelper.GetCamera(this._camera, gameObject);
 
-				// Add the deltaPosition
-				screenPoint += (Vector3)screenDelta * Sensitivity;
+            if (camera != null)
+            {
+                // Screen position of the transform
+                var screenPoint = camera.WorldToScreenPoint(transform.position);
 
-				// Convert back to world space
-				transform.position = camera.ScreenToWorldPoint(screenPoint);
-			}
-			else
-			{
-				Debug.LogError("Failed to find camera. Either tag your camera as MainCamera, or set one in this component.", this);
-			}
-		}
-	}
+                // Add the deltaPosition
+                screenPoint += (Vector3)screenDelta * Sensitivity;
+
+                // Convert back to world space
+                transform.position = camera.ScreenToWorldPoint(screenPoint);
+            }
+            else
+            {
+                Debug.LogError(
+                    "Failed to find camera. Either tag your camera as MainCamera, or set one in this component.",
+                    this
+                );
+            }
+        }
+    }
 }
 
 #if UNITY_EDITOR
 namespace Lean.Touch.Editor
 {
-	using UnityEditor;
-	using TARGET = LeanDragTranslate;
+    using UnityEditor;
+    using TARGET = LeanDragTranslate;
 
-	[CanEditMultipleObjects]
-	[CustomEditor(typeof(TARGET), true)]
-	public class LeanDragTranslate_Editor : CwEditor
-	{
-		protected override void OnInspector()
-		{
-			TARGET tgt; TARGET[] tgts; GetTargets(out tgt, out tgts);
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(TARGET), true)]
+    public class LeanDragTranslate_Editor : CwEditor
+    {
+        protected override void OnInspector()
+        {
+            TARGET tgt;
+            TARGET[] tgts;
+            GetTargets(out tgt, out tgts);
 
-			Draw("Use");
-			Draw("_camera", "The camera the translation will be calculated using.\n\nNone/null = MainCamera.");
-			Draw("sensitivity", "The movement speed will be multiplied by this.\n\n-1 = Inverted Controls.");
-			Draw("damping", "If you want this component to change smoothly over time, then this allows you to control how quick the changes reach their target value.\n\n-1 = Instantly change.\n\n1 = Slowly change.\n\n10 = Quickly change.");
-			Draw("inertia", "This allows you to control how much momentum is retained when the dragging fingers are all released.\n\nNOTE: This requires <b>Damping</b> to be above 0.");
-		}
-	}
+            Draw("Use");
+            Draw(
+                "_camera",
+                "The camera the translation will be calculated using.\n\nNone/null = MainCamera."
+            );
+            Draw(
+                "sensitivity",
+                "The movement speed will be multiplied by this.\n\n-1 = Inverted Controls."
+            );
+            Draw(
+                "damping",
+                "If you want this component to change smoothly over time, then this allows you to control how quick the changes reach their target value.\n\n-1 = Instantly change.\n\n1 = Slowly change.\n\n10 = Quickly change."
+            );
+            Draw(
+                "inertia",
+                "This allows you to control how much momentum is retained when the dragging fingers are all released.\n\nNOTE: This requires <b>Damping</b> to be above 0."
+            );
+        }
+    }
 }
 #endif
