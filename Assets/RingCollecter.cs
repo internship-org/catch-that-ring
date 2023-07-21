b97b524f295260c46f7f6b5fae51156522857b33
yigitkalan <sametykalan@gmail.com>
2023-07-21 12:48:14 +0300

    finish ring collector

--- Assets/RingCollecter.cs
+++ Assets/RingCollecter.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Lean.Pool;
 using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
-public class RingCollecter : MonoBehaviour { 
-
+public class RingCollecter : MonoBehaviour
+{
+    [SerializeField]
     Collider ringCollectTrigger;
-    private void Awake() {
+    private string ringTag = "RingTrigger";
+
+    private void Awake()
+    {
         ringCollectTrigger = GetComponent<Collider>();
     }
-    private void Start() {
-        ringCollectTrigger.OnTiggerEnterAsObservable()
+
+    private void Start()
+    {
+        ringCollectTrigger
+            .OnTriggerEnterAsObservable()
+            .Where(collider => collider.CompareTag(ringTag))
+            .Subscribe(collider =>
+            {
+                ScoreManager.Instance.AddScore();
+                LeanPool.Despawn(collider.transform.parent.gameObject);
+            })
+            .AddTo(this);
     }
 }
