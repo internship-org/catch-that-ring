e3af07b0d9072947cb8245e1cf22658d97b7f6ad
yigitkalan <sametykalan@gmail.com>
2023-07-25 16:05:14 +0300

    add events, conditions and listeners with atom for health

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -8,8 +8,10 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField]
     private IntVariable Health;
+    Image healthBarImage;
 
     Tween barTween;
+    Tween colorTween;
 
     private void Awake()
     {
@@ -18,40 +20,38 @@ public class HealthBar : MonoBehaviour
 
     private void Start()
     {
-        Health
-            .ObserveChange()
-            .Subscribe(health =>
-            {
-                var healthBarImage = GetComponent<Image>();
-                barTween = DOTween.To(
-                    () => healthBarImage.fillAmount,
-                    x => healthBarImage.fillAmount = x,
-                    1.0f * health / Health.InitialValue,
-                    1f
-                );
-                if (health < 2)
-                {
-                    healthBarImage.DOColor(Color.red, 1f);
-                }
-                else if (health < 4)
-                {
-                    healthBarImage.DOColor(Color.yellow, 1f);
-                }
-                else
-                {
-                    healthBarImage.DOColor(Color.green, 1f);
-                }
-            })
-            .AddTo(this);
+        healthBarImage = GetComponent<Image>();
+    }
 
-        Observable
-            .EveryUpdate()
-            .Where(_ => Input.GetKeyDown(KeyCode.Space))
-            .Subscribe(_ => Health.Value -= 1);
+    public void AnimateHealthBar()
+    {
+        barTween = DOTween.To(
+            () => healthBarImage.fillAmount,
+            x => healthBarImage.fillAmount = x,
+            1.0f * Health.Value / Health.InitialValue,
+            1f
+        );
+    }
+
+    public void AnimateBarColor()
+    {
+        if (Health.Value < 2)
+        {
+            colorTween = healthBarImage.DOColor(Color.red, 1f);
+        }
+        else if (Health.Value < 4)
+        {
+            colorTween = healthBarImage.DOColor(Color.yellow, 1f);
+        }
+        else
+        {
+            colorTween = healthBarImage.DOColor(Color.green, 1f);
+        }
     }
 
     private void OnDisable()
     {
+        colorTween.Kill();
         barTween.Kill();
     }
 }
