b197b639d5cfea870a9d186c84347cce2c2d7fdf
yettyo <yenerkaraca12@gmail.com>
2023-07-26 14:59:54 +0300

    Implemented SlowMotion powerup

--- Assets/Scripts/Rings/PowerUpRing.cs
+++ Assets/Scripts/Rings/PowerUpRing.cs
@@ -2,7 +2,9 @@ public class PowerUpRing : RingBase
 {
     public override void ApplyEffect()
     {
-        base.ApplyEffect();  
+        base.ApplyEffect();
+        SlowMotionApplier slowMotionApplier = FindObjectOfType<SlowMotionApplier>();
+        slowMotionApplier.ApplySlowMo();
     }
 
     public override void OnMissed()
---
+++ Assets/Scripts/SlowMotionApplier.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+public class SlowMotionApplier : MonoBehaviour
+{
+    private string targetTag = "Ring";
+    private float SlowmotionValue = 0.5f;
+    private float normaltimeScale = 1f;
+    private bool isSlowMo = false;
+    public void ApplySlowMo()
+    {
+        if (gameObject.CompareTag(targetTag))
+        {
+            Time.timeScale = SlowmotionValue;
+            //Time.fixedDeltaTime = Time.timeScale * .02f;
+            if(!isSlowMo) {
+                isSlowMo = true;
+                StartCoroutine(ResetSlowMo());
+            }
+            
+        }
+        else
+        {
+            Time.timeScale = normaltimeScale;
+        }
+    }
+
+    private IEnumerator ResetSlowMo() {
+        yield return new WaitForSeconds(1f);
+        Time.timeScale = normaltimeScale;
+        isSlowMo = false;
+    }
+}
