3c8bbfcfd7716b538f737235c85509b4839d2e6a
yettyo <yenerkaraca12@gmail.com>
2023-07-26 12:56:28 +0300

    added TimeManager for Slow motion

--- Assets/Scripts/Rings/PowerUpRing.cs
+++ Assets/Scripts/Rings/PowerUpRing.cs
@@ -1,8 +1,15 @@
 public class PowerUpRing : RingBase
 {
+    private TimeManager timeManager;
+
+    private void Start() {
+        timeManager = FindObjectOfType<TimeManager>();
+    }
     public override void ApplyEffect()
     {
         base.ApplyEffect();
+        timeManager.DoSlowmotion();
+        
     }
 
     public override void OnMissed()
